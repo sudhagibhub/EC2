e6582322beffd13b8454d93a2143287c89c5dd5d
SudhakaranG <sudhakaran.go2105@gmail.com>
2025-07-03 19:46:37 +0530

    Update main.tf-by sudha

--- main.tf
+++ main.tf
@@ -1,8 +1,19 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
+provider "aws" {
+  region = "us-east-1" # Replace with your desired region
+}
+
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 5.0"
+
+  name = "my-ec2-instance"
+
+  instance_type = "t2.micro"
+  ami           = "ami-022bbd2ccaf21691f" # Replace with your desired AMI
+  key_name      = "ec2.pem"       # Replace with your key pair name
+  subnet_id     = "subnet-vpc-0bcd5417d45a7e7cc" # Replace with your subnet ID
+  vpc_security_group_ids = ["sg-launch-wizard-1"] # Replace with your security group IDs
+  tags = {
+    Name = "MyEC2Instance"
   }
 }

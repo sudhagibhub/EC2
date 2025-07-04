5a2f73f180c4884c37903e5f61c891c439c80692
SudhakaranG <sudhakaran.go2105@gmail.com>
2025-07-04 18:04:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ module "ec2_instance" {
   name = "my-ec2-instance"
 
   instance_type = "t2.micro"
-  ami           = "ami-02b60b5095d1e5227" # Replace with your desired AMI
+  ami           = "ami-05ffe3c48a9991133" # Replace with your desired AMI
   key_name      = "ec2.pem"       # Replace with your key pair name
   subnet_id     = "subnet-vpc-0bcd5417d45a7e7cc" # Replace with your subnet ID
   vpc_security_group_ids = ["launch-wizard-2"] # Replace with your security group IDs
